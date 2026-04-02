b791bddc25928ab1811e1046b65e267505cc8129
AJ Almaguer <ajalmaguer@gmail.com>
2026-04-02 02:20:43 -0700

    can deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pickleball_scroekeeper/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'node',
